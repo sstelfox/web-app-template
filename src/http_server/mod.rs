79a1f20a72336559687843cfaef90cb22cd41d98
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-08-08 10:10:47 -0400

    chore: some reorganization

--- src/http_server/mod.rs
+++ src/http_server/mod.rs
@@ -3,9 +3,7 @@ use std::time::Duration;
 
 use axum::error_handling::HandleErrorLayer;
 use axum::extract::DefaultBodyLimit;
-use axum::http::StatusCode;
-use axum::response::{IntoResponse, Response};
-use axum::{Json, Router};
+use axum::Router;
 use axum::{Server, ServiceExt};
 use http::header;
 use tower::ServiceBuilder;
@@ -20,9 +18,10 @@ use tracing::Level;
 
 use crate::app::{Config, Error, State};
 
-// todo: might want a longer timeout in some parts of the API and I'd like to be able customize a
-// few layers eventually such as CORS and request timeouts but that's for something down the line
 const REQUEST_TIMEOUT_SECS: u64 = 15;
+
+mod error_handlers;
+mod middleware;
 
 async fn graceful_shutdown_blocker() {
     use tokio::signal::unix;
@@ -37,38 +36,6 @@ async fn graceful_shutdown_blocker() {
         _ = sig_int_handler.recv() => tracing::debug!("gracefully exiting on an interrupt signal"),
         _ = sig_term_handler.recv() => tracing::debug!("gracefully exiting on an terminate signal"),
     }
-}
-
-async fn handle_error(error: tower::BoxError) -> Response {
-    if error.is::<tower::timeout::error::Elapsed>() {
-        let msg = serde_json::json!({"status": "error", "message": "request timed out"});
-        return (StatusCode::REQUEST_TIMEOUT, Json(msg)).into_response();
-    }
-
-    if error.is::<tower::load_shed::error::Overloaded>() {
-        let msg = serde_json::json!({"status": "error", "message": "service overloaded"});
-        return (StatusCode::SERVICE_UNAVAILABLE, Json(msg)).into_response();
-    }
-
-    let mut errors = vec![error.to_string()];
-    let mut source = error.source();
-
-    while let Some(inner_err) = source {
-        errors.push(inner_err.to_string());
-        source = inner_err.source();
-    }
-
-    tracing::error!(errors = ?errors, "unhandled error");
-
-    let msg = serde_json::json!({"status": "error", "message": "unknown server error"});
-    (StatusCode::INTERNAL_SERVER_ERROR, Json(msg)).into_response()
-}
-
-async fn not_found_handler() -> Response {
-    (
-        StatusCode::NOT_FOUND,
-        Json(serde_json::json!({"status": "not found"})),
-    ).into_response()
 }
 
 pub async fn run(config: Config) -> Result<(), Error> {
@@ -90,7 +57,7 @@ pub async fn run(config: Config) -> Result<(), Error> {
         .on_failure(DefaultOnFailure::new().latency_unit(LatencyUnit::Micros));
 
     let middleware_stack = ServiceBuilder::new()
-        .layer(HandleErrorLayer::new(handle_error))
+        .layer(HandleErrorLayer::new(error_handlers::server_error_handler))
         .load_shed()
         .concurrency_limit(1024)
         .timeout(Duration::from_secs(REQUEST_TIMEOUT_SECS))
@@ -106,16 +73,16 @@ pub async fn run(config: Config) -> Result<(), Error> {
             sensitive_headers,
         ));
 
+    tracing::info!(addr = ?config.listen_addr(), "server listening");
+
     let state = State::from_config(&config).await;
     let root_router = Router::new()
         //.nest("/api/v1", api::router(app_state.clone()))
         //.nest("/_status", health_check::router(app_state.clone()))
         .with_state(state)
-        .fallback(not_found_handler);
+        .fallback(error_handlers::not_found_handler);
 
     let app = middleware_stack.service(root_router);
-
-    tracing::info!(addr = ?config.listen_addr(), "server listening");
 
     Server::bind(config.listen_addr())
         .serve(app.into_make_service())
