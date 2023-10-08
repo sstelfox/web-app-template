a1960145d3d68569c0c2e6404e761fe2f986e528
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-10-08 18:26:55 -0400

    feat: well not quite as pretty as I wanted but that works

--- src/database/custom_types/user_id.rs
+++ src/database/custom_types/user_id.rs
@@ -2,11 +2,29 @@ use std::fmt::{self, Display, Formatter};
 
 use uuid::Uuid;
 
+use crate::database::Database;
 use crate::database::custom_types::Did;
 
 #[derive(Clone, Copy, Debug, sqlx::Type)]
 #[sqlx(transparent)]
 pub struct UserId(Did);
+
+impl UserId {
+    pub async fn from_email(database: &Database, email: &str) -> Result<Option<Self>, UserIdError> {
+        let maybe_id: Option<String> = sqlx::query_scalar!(
+            "SELECT id FROM users WHERE email = LOWER($1);",
+            email,
+        )
+        .fetch_optional(database)
+        .await
+        .map_err(UserIdError::LookupFailed)?;
+
+        match maybe_id {
+            Some(sid) => Ok(Some(UserId(Did::try_from(sid).map_err(UserIdError::CorruptId)?))),
+            None => Ok(None),
+        }
+    }
+}
 
 impl Display for UserId {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
@@ -16,7 +34,7 @@ impl Display for UserId {
 
 impl From<String> for UserId {
     fn from(val: String) -> Self {
-        Self(Did::try_from(val).expect("user ID to be valid"))
+        Self(Did::try_from(val).expect("valid user ID"))
     }
 }
 
@@ -24,4 +42,13 @@ impl From<Uuid> for UserId {
     fn from(val: Uuid) -> Self {
         Self(Did::from(val))
     }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum UserIdError {
+    #[error("user ID present in a database is corrupt: {0}")]
+    CorruptId(uuid::Error),
+
+    #[error("failed to lookup user ID: {0}")]
+    LookupFailed(sqlx::Error),
 }
