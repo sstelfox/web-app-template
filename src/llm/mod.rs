16ee9302dc2b4795af0f2c4e5a82edeaec346445
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-10-23 08:21:03 -0400

    feat: get model checking

--- src/llm/mod.rs
+++ src/llm/mod.rs
@@ -5,9 +5,13 @@ pub mod hugging_face {
 
     const SAFE_TENSOR_REPO_FMT: &str = "https://huggingface.co/{}/resolve/main/model.safetensors";
 
-    pub struct ModelVersion(String);
+    pub struct ModelVersion {
+        commit: String,
+        etag: String,
+        size: usize,
+    }
 
-    use reqwest::header::{HeaderMap, HeaderValue};
+    use reqwest::header::{CONTENT_RANGE, LOCATION, RANGE, HeaderMap, HeaderName, HeaderValue, ToStrError};
     use reqwest::redirect::Policy;
 
     fn no_redirect_client() -> Result<reqwest::Client, HuggingFaceError> {
@@ -17,41 +21,94 @@ pub mod hugging_face {
         let client = reqwest::Client::builder()
             .default_headers(default_headers)
             .redirect(Policy::none())
-            .user_agent("test-app-client/0.1.0")
+            .user_agent(format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")))
             .build()
             .map_err(HuggingFaceError::BuildError)?;
 
         Ok(client)
     }
 
+    fn header(name: HeaderName, headers: &HeaderMap) -> Result<String, HuggingFaceError> {
+        headers
+            .get(name)
+            .ok_or(HuggingFaceError::MissingHeader)?
+            .to_str()
+            .map_err(HuggingFaceError::InvalidHeaderValue)
+            .map(|v| v.to_string())
+    }
+
     pub async fn check_safetensor_model_version(model: &str) -> Result<ModelVersion, HuggingFaceError> {
+        let client = no_redirect_client()?;
+
         let model_url = SAFE_TENSOR_REPO_FMT.replace("{}", model);
 
-        let mut headers = HeaderMap::new();
-        headers.insert("Range", HeaderValue::from_static("bytes=0-0"));
-
-        let client = no_redirect_client()?;
-        let request = client
-            .get(model_url)
-            .headers(headers);
-
-        let response = request
+        let mut response = client
+            .get(&model_url)
             .send()
             .await
-            .map_err(HuggingFaceError::VersionCheckError)?;
+            .map_err(HuggingFaceError::NoMetadata)?;
 
-        println!("Response: {:?} {}", response.version(), response.status());
-        println!("Headers: {:#?}\n", response.headers());
+        let metadata_headers = response.headers();
 
-        todo!()
+        // Try and use the custom X-Linked-Etag header to get the cache key for this model falling
+        // back on the standard Etag header if its not present
+        let etag = match metadata_headers.get(HeaderName::from_static("X-Linked-Etag")) {
+            Some(e) => e,
+            None => {
+                metadata_headers
+                    .get(HeaderName::from_static("Etag"))
+                    .ok_or(HuggingFaceError::MissingHeader)?
+            }
+        };
+        let etag = etag.to_str().map_err(HuggingFaceError::InvalidHeaderValue)?.to_string().replace('"', "");
+
+        // The commit level is also in a custom header
+        let current_commit = header(HeaderName::from_static("X-Repo-Commit"), &metadata_headers)?;
+
+        if response.status().is_redirection() {
+            let next_location = header(LOCATION, &metadata_headers)?;
+
+            response = client
+                .get(&model_url)
+                // We don't actually want the data, indicate as much
+                .header(RANGE, "bytes=0-0")
+                .send()
+                .await
+                .map_err(HuggingFaceError::RedirectFailed)?;
+        }
+
+        let content_range = header(CONTENT_RANGE, response.headers())?;
+
+        let size = content_range.split('/').last().ok_or(HuggingFaceError::BadContentRange)?.parse().map_err(HuggingFaceError::InvalidSize)?;
+
+        Ok(ModelVersion {
+            commit: current_commit,
+            etag,
+            size,
+        })
     }
 
     #[derive(Debug, thiserror::Error)]
     pub enum HuggingFaceError {
+        #[error("bad format for content range header")]
+        BadContentRange,
+
         #[error("error occurred building a client: {0}")]
         BuildError(reqwest::Error),
 
-        #[error("unable to check model version: {0}")]
-        VersionCheckError(reqwest::Error),
+        #[error("expected a header to be a valid string")]
+        InvalidHeaderValue(ToStrError),
+
+        #[error("the provided content size wasn't a number")]
+        InvalidSize(std::num::ParseIntError),
+
+        #[error("a required header was missing")]
+        MissingHeader,
+
+        #[error("unable to make first metadata request: {0}")]
+        NoMetadata(reqwest::Error),
+
+        #[error("attempting to follow the provided redirect failed: {0}")]
+        RedirectFailed(reqwest::Error),
     }
 }
